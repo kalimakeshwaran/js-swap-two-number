50d6e9a091bd3f0dc670520656b8c087e8bb2c1a
kalimakeshwaran <makeshvijay735@gmail.com>
2022-12-16 17:00:31 +0530

    add

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-
-
-
-
-
 var a,b,c;
-var a=promt("enter the value")
-var b=promt("enter the value")
+a=prompt("enter the value")
+b=prompt("enter the number")
+document.write("A=" +a) 
+document.write("b= "+b+"<br>")
 
 function swap(a,b){
     c=a;
     a=b;
     b=c;
-    document.writeswap(a,b)
-   
-    
+    document.write("A=" +a) 
+    document.write("b= "+b) 
 }
+swap(a,b)
